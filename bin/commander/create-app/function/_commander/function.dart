b6590f49e79bcbca316e9541d4cf2dcb32111f69
june lee <melodysdreamj@gmail.com>
2024-03-18 23:12:10 +0900

    `

--- bin/commander/create-app/function/_commander/function.dart
+++ bin/commander/create-app/function/_commander/function.dart
@@ -3,6 +3,7 @@ import '../../../../entity/model/creation_result/model.dart';
 import '../../../../util/clone_and_remove_git/function.dart';
 import '../ask_user_input_for_project_creation/function.dart';
 import '../change_project_name/function.dart';
+import '../rename_new_folder/function.dart';
 import '../replace_sttring_in_files/function.dart';
 
 createApp() async {
@@ -27,7 +28,31 @@ createApp() async {
   if (result.Type == ProjectTypeEnum.Skeleton) {
     await replaceStringInFiles(
         result.Name, 'june.lee.love', result.PackageName);
+  } else {
+    await replaceStringInFiles('${result.Name}/lib/util/_/initial_app', 'New',
+        _toPascalCase(result.Name));
+
+    await renameNewFolders('${result.Name}/lib/util', result.Name);
+
+    await replaceStringInFiles(
+        '${result.Name}/juneflow_module.yaml', '_new', result.Name);
   }
 
   print('\nCongratulations! Your project has been created successfully!');
+}
+
+String _toPascalCase(String text) {
+  // 언더스코어로 단어를 분리하여 리스트를 생성
+  List<String> words = text.split('_');
+
+  // 모든 단어의 첫 글자를 대문자로 변환
+  for (int i = 0; i < words.length; i++) {
+    if (words[i].isNotEmpty) {
+      words[i] =
+          words[i][0].toUpperCase() + words[i].substring(1).toLowerCase();
+    }
+  }
+
+  // 단어들을 다시 하나의 문자열로 합침
+  return words.join('');
 }
---
+++ bin/commander/create-app/function/rename_new_folder/function.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+Future<void> renameNewFolders(String directoryPath, String newName) async {
+  final Directory directory = Directory(directoryPath);
+
+  if (!await directory.exists()) {
+    print('The specified directory does not exist.');
+    return;
+  }
+
+  await for (final FileSystemEntity entity in directory.list(recursive: true)) {
+    if (entity is Directory) {
+      final String dirName = p.basename(entity.path);
+      if (dirName == '_new') {
+        final String newPath = entity.path.replaceFirst(RegExp(r'_new$'), newName);
+        await entity.rename(newPath);
+        print('Renamed ${entity.path} to $newPath');
+      }
+    }
+  }
+}
