ece5817cc496539bfa63c6a5055de92c896df8c7
june lee <melodysdreamj@gmail.com>
2024-03-21 20:03:19 +0900

    Update function.dart

--- bin/commander/build/function/add_package_in_modules/function.dart
+++ bin/commander/build/function/add_package_in_modules/function.dart
@@ -3,7 +3,7 @@ import '../flutter_package_add/function.dart';
 
 Future<void> addPackageInModules(List<PackageInfo> packages) async {
   for(PackageInfo package in packages) {
-    print('Add package: ${package.Name} in modules');
+    // print('Add package: ${package.Name} in modules');
     await addFlutterPackage(package.Name, version: package.Version);
   }
 }
