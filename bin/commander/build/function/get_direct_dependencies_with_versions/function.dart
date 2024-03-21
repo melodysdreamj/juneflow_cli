6eb694d9d221af50dea1f0e1dfe68eda3c78bac2
june lee <melodysdreamj@gmail.com>
2024-03-22 05:06:09 +0900

    Update function.dart

--- bin/commander/build/function/get_direct_dependencies_with_versions/function.dart
+++ bin/commander/build/function/get_direct_dependencies_with_versions/function.dart
@@ -7,25 +7,30 @@ Future<List<PackageInfo>> getDirectDependenciesWithVersions(String packagePath) 
   final pubspecYaml = File('$packagePath/pubspec.yaml');
   final pubspecLock = File('pubspec.lock');
 
-  // `pubspec.yaml`에서 직접 의존성 이름 추출
-  final pubspecContents = await pubspecYaml.readAsString();
-  // print('pubspecContents: $pubspecContents');
-  final pubspec = loadYaml(pubspecContents);
-  // print("pubspec['dependencies']: ${pubspec['dependencies']}");
-  final directDependencies = pubspec['dependencies'] as YamlMap;
+  // `pubspec.yaml`에서 직접 의존성 이름과 주석을 추출
+  final pubspecContents = await pubspecYaml.readAsLines();
+  final directDependenciesWithComments = {};
+  bool inDependenciesSection = false;
+  for (var line in pubspecContents) {
+    if (line.trim() == 'dependencies:') {
+      inDependenciesSection = true;
+    } else if (inDependenciesSection && line.trim().isEmpty) {
+      inDependenciesSection = false; // End of dependencies section
+    } else if (inDependenciesSection && line.contains('//@add')) {
+      final dependencyName = line.split(':')[0].trim();
+      directDependenciesWithComments[dependencyName] = true;
+    }
+  }
 
-  // `pubspec.module`에서 버전 정보 추출
+  // `pubspec.lock`에서 버전 정보 추출
   final pubspecLockContents = await pubspecLock.readAsString();
   final lockfile = loadYaml(pubspecLockContents);
   final packages = lockfile['packages'] as YamlMap;
 
   final List<PackageInfo> dependenciesWithVersions = [];
-  for (var packageName in directDependencies.keys) {
-    // print('packageName: $packageName');
-    if (directDependencies[packageName] is! YamlMap) {
-      final version = packages.containsKey(packageName) ? (packages[packageName] as YamlMap)['version'] as String : 'Unknown Version';
-      dependenciesWithVersions.add(PackageInfo()..Name = packageName..Version = version);
-    }
+  for (var packageName in directDependenciesWithComments.keys) {
+    final version = packages.containsKey(packageName) ? (packages[packageName] as YamlMap)['version'] as String : 'Unknown Version';
+    dependenciesWithVersions.add(PackageInfo()..Name = packageName..Version = version);
   }
 
   return dependenciesWithVersions;
@@ -33,28 +38,34 @@ Future<List<PackageInfo>> getDirectDependenciesWithVersions(String packagePath) 
 
 
 Future<List<PackageInfo>> getDirectDevDependenciesWithVersions(String packagePath) async {
+
   final pubspecYaml = File('$packagePath/pubspec.yaml');
   final pubspecLock = File('pubspec.lock');
 
-  // `pubspec.yaml`에서 직접 의존성 이름 추출
-  final pubspecContents = await pubspecYaml.readAsString();
-  // print('pubspecContents: $pubspecContents');
-  final pubspec = loadYaml(pubspecContents);
-  // print("pubspec['dependencies']: ${pubspec['dependencies']}");
-  final directDependencies = pubspec['dev_dependencies'] as YamlMap;
+  // `pubspec.yaml`에서 직접 의존성 이름과 주석을 추출
+  final pubspecContents = await pubspecYaml.readAsLines();
+  final directDependenciesWithComments = {};
+  bool inDependenciesSection = false;
+  for (var line in pubspecContents) {
+    if (line.trim() == 'dev_dependencies:') {
+      inDependenciesSection = true;
+    } else if (inDependenciesSection && line.trim().isEmpty) {
+      inDependenciesSection = false; // End of dependencies section
+    } else if (inDependenciesSection && line.contains('//@add')) {
+      final dependencyName = line.split(':')[0].trim();
+      directDependenciesWithComments[dependencyName] = true;
+    }
+  }
 
-  // `pubspec.module`에서 버전 정보 추출
+  // `pubspec.lock`에서 버전 정보 추출
   final pubspecLockContents = await pubspecLock.readAsString();
   final lockfile = loadYaml(pubspecLockContents);
   final packages = lockfile['packages'] as YamlMap;
 
   final List<PackageInfo> dependenciesWithVersions = [];
-  for (var packageName in directDependencies.keys) {
-    // print('packageName: $packageName');
-    if (directDependencies[packageName] is! YamlMap) {
-      final version = packages.containsKey(packageName) ? (packages[packageName] as YamlMap)['version'] as String : 'Unknown Version';
-      dependenciesWithVersions.add(PackageInfo()..Name = packageName..Version = version);
-    }
+  for (var packageName in directDependenciesWithComments.keys) {
+    final version = packages.containsKey(packageName) ? (packages[packageName] as YamlMap)['version'] as String : 'Unknown Version';
+    dependenciesWithVersions.add(PackageInfo()..Name = packageName..Version = version);
   }
 
   return dependenciesWithVersions;
