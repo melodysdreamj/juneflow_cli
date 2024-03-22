41c6f3b8ad062232171fea301059323b88a27a4f
june lee <melodysdreamj@gmail.com>
2024-03-22 22:10:49 +0900

    `

--- bin/commander/add/function/_/function.dart
+++ bin/commander/add/function/_/function.dart
@@ -1,10 +1,25 @@
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
 import '../../../build/function/_/function.dart';
 import '../../../build/function/add_package_in_modules/function.dart';
 import '../../../build/function/flutter_package_add/function.dart';
 import '../../../build/function/flutter_pub_get/function.dart';
 
 addModule(String moduleName) async {
-  await addFlutterPackage(moduleName, devPackage: true);
+  // await checkAndAddModules(moduleName, devPackage: true);
+
+  final file = File('pubspec.yaml');
+  final yamlString = await file.readAsString();
+  YamlMap yamlDoc = loadYaml(yamlString);
+  bool isDuplicated = await isPackageDuplicated(yamlDoc, moduleName,
+      devPackage: true);
+  if (!isDuplicated) {
+    await addFlutterPackage(moduleName,
+        devPackage: true);
+  }
+
 
   await runFlutterPubGet();
 
--- bin/commander/build/function/_/function.dart
+++ bin/commander/build/function/_/function.dart
@@ -62,8 +62,8 @@ buildApp() async {
     // await addPackageInModules(module.Packages);
     // await addDevPackageInModules(module.DevPackage);
 
-    await checkAndAddModules(module.Packages, devPackages: false);
-    await checkAndAddModules(module.DevPackage, devPackages: true);
+    await addPackagesIfNeeded(module.Packages, devPackage: false);
+    await addPackagesIfNeeded(module.DevPackage, devPackage: true);
   }
 
   // 7. apply .tempDir to lib folder
