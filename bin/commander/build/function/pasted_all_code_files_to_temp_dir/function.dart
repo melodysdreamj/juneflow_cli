124c8989d26ae1c595ca58527f32f102147ac68b
june lee <melodysdreamj@gmail.com>
2024-03-22 17:30:07 +0900

    Update function.dart

--- bin/commander/build/function/pasted_all_code_files_to_temp_dir/function.dart
+++ bin/commander/build/function/pasted_all_code_files_to_temp_dir/function.dart
@@ -14,7 +14,7 @@ Future<void> pasteAllCodeFiles(
     if (await newFile.exists()) {
       // 파일이 이미 존재하면 에러 메시지를 출력하고 함수 실행을 종료
       // print('\x1B[31mError: The $libraryName conflicts with another module.\x1B[0m');
-      throw Exception('The $libraryName conflicts with another module.');
+      throw Exception('The $libraryName conflicts with another module. filename : ${fileObj.Path}');
     }
 
     // 파일이 존재하지 않으면, 새로 생성하고 내용을 작성
