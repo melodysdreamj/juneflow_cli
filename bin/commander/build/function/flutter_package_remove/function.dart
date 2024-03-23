a03d6a8553567a9d198f1e4941d55a5df5034c1d
june lee <melodysdreamj@gmail.com>
2024-03-23 17:24:24 +0900

    Update function.dart

--- bin/commander/build/function/flutter_package_remove/function.dart
+++ bin/commander/build/function/flutter_package_remove/function.dart
@@ -1,23 +1,15 @@
 import 'dart:io';
 
 // 패키지를 입력받아 flutter pub remove 명령을 실행하는 함수
-Future<void> removeFlutterPackage(String packageName, {bool? devPackage = false}) async {
-  // devPackage가 true일 경우 '--dev' 옵션을 추가합니다.
-  final List<String> command = ['pub', 'remove', packageName];
-  if (devPackage == true) {
-    command.add('--dev');
-  }
-
-  // 프로세스를 실행하여 패키지를 제거합니다.
-  final result = await Process.run('flutter', command,
+Future<void> removeFlutterPackage(String packageName) async {
+  final result = await Process.run('flutter', ['pub', 'remove', packageName],
       workingDirectory: Directory.current.path // 현재 작업 중인 디렉토리를 사용합니다.
-  );
+      );
 
-  // 실행 결과를 출력합니다.
-  if (result.stderr.toString().isNotEmpty) {
-    print('error: ${result.stderr}');
-  } else {
-    final String devStr = (devPackage == true) ? 'dev_dependencies' : 'dependencies';
-    print("Removed $packageName from $devStr.");
-  }
+  // 실행 결과 출력
+  // print('Exit code: ${result.exitCode}');
+  // print('Stdout: ${result.stdout}');
+  // print('Stderr: ${result.stderr}');
+
+  print("removed $packageName");
 }
