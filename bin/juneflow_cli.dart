ad79d8dfd28cb94739d78748f57c9d730e8578ec
june lee <melodysdreamj@gmail.com>
2024-03-23 17:19:54 +0900

    Update juneflow_cli.dart

--- bin/juneflow_cli.dart
+++ bin/juneflow_cli.dart
@@ -88,13 +88,15 @@ void main(List<String> arguments) {
         case 'build':
           print('Project initialization process initiated.');
           buildApp();
+          print('Project initialization process completed.');
           break;
         case 'add':
           if (results.rest.length > 1) {
             // 'add' 명령어와 함께 패키지명 처리
             String moduleName = results.rest[1];
-            print('Adding module: $moduleName');
+            print("start add module: $moduleName")
             addModule(moduleName);
+            print("end add module: $moduleName")
           } else {
             print('Package name is missing.');
           }
