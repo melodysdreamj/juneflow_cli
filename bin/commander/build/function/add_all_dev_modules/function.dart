fdab18320ea50631489bde41a6be0487f8923b42
june lee <melodysdreamj@gmail.com>
2024-03-25 08:30:56 +0900

    Update function.dart

--- bin/commander/build/function/add_all_dev_modules/function.dart
+++ bin/commander/build/function/add_all_dev_modules/function.dart
@@ -49,6 +49,7 @@ Future<void> addPackageUsingPath(String packagePath) async {
   }
 
   for (var package in devPackagesInfo) {
+    print('Adding dev ${package.Name}...')
     bool isExistBefore = await addFlutterPackage(package.Name,
         version: package.Version, devPackage: true);
     String? _packagePath = getPackagePath(package.Name, package.Version);
