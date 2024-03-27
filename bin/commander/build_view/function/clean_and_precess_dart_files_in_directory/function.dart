5ea83b65adaa7376b5f18406cd4b41743dc06cde
june lee <melodysdreamj@gmail.com>
2024-03-27 15:11:30 +0900

    Update function.dart

--- bin/commander/build_view/function/clean_and_precess_dart_files_in_directory/function.dart
+++ bin/commander/build_view/function/clean_and_precess_dart_files_in_directory/function.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
 Future<void> cleanAndProcessDartFilesInDirectory(String directoryPath) async {
   final directory = Directory(directoryPath);
   await for (final FileSystemEntity entity in directory.list(recursive: true)) {
@@ -15,23 +18,32 @@ Future<void> cleanImportsAndGeneratedCodeInFile(String filePath) async {
   List<String> lines = fileContent.split('\n');
 
   // @JuneViewChild() 또는 @JuneViewMother()가 있는지 확인
-  bool hasTargetLines = lines.any((line) =>
-      line.startsWith('@JuneViewChild()') ||
-      line.startsWith('@JuneViewMother()'));
+  bool hasTargetLines = lines.any((line) => line.startsWith('@JuneViewChild()') || line.startsWith('@JuneViewMother()'));
   if (!hasTargetLines) return; // 해당하는 줄이 없으면 아무 작업도 수행하지 않음
 
-  // 필요한 import 외 삭제
+  // 필요한 import 목록
   final requiredImports = [
     "import 'package:flutter/cupertino.dart';",
     "import 'package:flutter/material.dart';",
     "import '../../../../../../../../main.dart';",
     "import '../view.dart';",
   ];
-  lines.retainWhere((line) =>
-      line.startsWith('@JuneViewChild()') ||
-      line.startsWith('@JuneViewMother()') ||
-      requiredImports.contains(line.trim()) ||
-      !line.startsWith('import '));
+
+  // 이미 존재하는 import 확인
+  List<String> existingImports = [];
+  lines.retainWhere((line) {
+    bool isImportLine = line.startsWith('import ');
+    if (isImportLine && requiredImports.contains(line.trim())) {
+      existingImports.add(line.trim());
+    }
+    return !isImportLine || requiredImports.contains(line.trim());
+  });
+
+  // 필요한 import 중 누락된 것이 있으면 추가
+  List<String> missingImports = requiredImports.where((import) => !existingImports.contains(import)).toList();
+  if (missingImports.isNotEmpty) {
+    lines.insertAll(0, missingImports);
+  }
 
   // 자동 생성된 코드 블록 처리
   removeContentBetweenGeneratedCodeMarkers(lines, 'action');
@@ -42,8 +54,7 @@ Future<void> cleanImportsAndGeneratedCodeInFile(String filePath) async {
 }
 
 void removeContentBetweenGeneratedCodeMarkers(List<String> lines, String type) {
-  const startMarker =
-      '/// automatically generated {type} code - don\'t change this code';
+  const startMarker = '/// automatically generated {type} code - don\'t change this code';
   const endMarker = '/// end of automatically {type} generated code';
   int startIndex = -1;
   int endIndex = -1;
