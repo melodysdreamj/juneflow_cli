183a3c6a2017dab52b1c9a692c585100b91676f9
june lee <melodysdreamj@gmail.com>
2024-03-27 09:05:34 +0900

    Update function.dart

--- bin/commander/build_view/function/find_files_with_june_view_annotations/function.dart
+++ bin/commander/build_view/function/find_files_with_june_view_annotations/function.dart
@@ -8,9 +8,17 @@ Future<List<String>> findFilesWithJuneViewAnnotations(String directoryPath) asyn
     await for (var entity in directory.list(recursive: false, followLinks: false)) {
       if (entity is File) {
         // 파일인 경우 내용을 검사
-        String content = await entity.readAsString();
-        if (content.contains('@JuneViewAction()') || content.contains('@JuneViewEvent()')) {
-          filesWithAnnotations.add(entity.path);
+        try {
+          String content = await entity.readAsString();
+          if (content.contains('@JuneViewAction()') || content.contains('@JuneViewEvent()')) {
+            filesWithAnnotations.add(entity.path);
+          }
+        } catch (e) {
+          if (e is FileSystemException) {
+            // print("읽을 수 없는 파일을 건너뜁니다: ${entity.path}");
+          } else {
+            throw e; // 알려지지 않은 다른 종류의 예외는 다시 던집니다.
+          }
         }
       } else if (entity is Directory) {
         // 디렉토리인 경우 재귀적으로 탐색
