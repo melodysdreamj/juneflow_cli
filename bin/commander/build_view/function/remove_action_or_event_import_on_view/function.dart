c14490484dc20167431d873674d7da9c0b8a7629
june lee <melodysdreamj@gmail.com>
2024-03-30 14:41:30 +0900

    ₩

--- bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
+++ bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
@@ -1,19 +1,26 @@
 import 'dart:io';
+import 'package:path/path.dart' as p;
 
 Future<void> removeActionOrEventImportOnView(String path) async {
-  // 파일에서 각 줄을 읽어들입니다.
-  List<String> lines = await File(path).readAsLines();
+  // path에서 상위 폴더의 경로를 가져옵니다.
+  String directoryPath = p.dirname(path);
+
+  // 상위 폴더 내의 view.dart 파일의 경로를 구성합니다.
+  String viewPath = p.join(directoryPath, 'view.dart');
+
+  // view.dart 파일에서 각 줄을 읽어들입니다.
+  List<String> lines = await File(viewPath).readAsLines();
 
   // 'action/' 또는 'event/'로 시작하는 import와 export 구문을 제외하고 필터링합니다.
   List<String> filteredLines = lines
       .where((line) => !(line.startsWith("import 'action/") ||
-          line.startsWith("import 'event/") ||
-          line.startsWith("export 'action/") ||
-          line.startsWith("export 'event/")))
+      line.startsWith("import 'event/") ||
+      line.startsWith("export 'action/") ||
+      line.startsWith("export 'event/")))
       .toList();
 
-  // 필터링된 내용으로 파일을 다시 씁니다.
-  await File(path).writeAsString(filteredLines.join('\n'));
+  // 필터링된 내용으로 view.dart 파일을 다시 씁니다.
+  await File(viewPath).writeAsString(filteredLines.join('\n'));
 }
 
 void main() {
