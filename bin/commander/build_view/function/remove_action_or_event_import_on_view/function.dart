4564a00c2f1b99e85293928f8b7be30b952b0157
june lee <melodysdreamj@gmail.com>
2024-03-30 15:28:56 +0900

    Update function.dart

--- bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
+++ bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
@@ -11,16 +11,22 @@ Future<void> removeActionOrEventImportOnView(String path) async {
   // view.dart 파일에서 각 줄을 읽어들입니다.
   List<String> lines = await File(viewPath).readAsLines();
 
-  // 파일의 마지막 줄이 비어 있는지 체크합니다.
+  // 파일의 마지막 줄이 비어 있는지 확인합니다.
   bool endsWithEmptyLine = lines.isNotEmpty && lines.last.isEmpty;
 
-  // 'import' 또는 'export'로 시작하는 줄 중, 'action/' 또는 'event/'로 시작하는 경우 제외
-  List<String> filteredLines = lines.where((line) =>
-  !(line.startsWith("import 'action/") || line.startsWith("import 'event/") ||
-      line.startsWith("export 'action/") || line.startsWith("export 'event/"))).toList();
+  // 오직 'import ' 또는 'export '로 시작하는 줄만 필터링합니다.
+  // 그리고 'action/' 또는 'event/'로 시작하는 경우에만 제외합니다.
+  List<String> filteredLines = lines.map((line) {
+    if (line.startsWith("import ") || line.startsWith("export ")) {
+      if (line.contains(" 'action/") || line.contains(" 'event/")) {
+        return null; // 이 조건에 해당하는 줄은 제거합니다.
+      }
+    }
+    return line; // 나머지 줄들은 변경하지 않고 유지합니다.
+  }).whereType<String>().toList(); // null이 아닌 줄들만 다시 리스트로 만듭니다.
 
-  // 필터링된 내용으로 파일을 다시 씁니다.
-  // 만약 원본 파일의 마지막 줄이 비어 있었다면, 저장할 때 그 상태를 유지합니다.
+  // 필터링된 내용으로 view.dart 파일을 다시 씁니다.
+  // 파일의 마지막 줄이 비어 있었다면, 저장할 때 그 상태를 유지합니다.
   String contentToWrite = filteredLines.join('\n');
   if (endsWithEmptyLine) {
     contentToWrite += '\n'; // 마지막 줄이 비어 있었다면, 추가적인 줄바꿈을 추가합니다.
