08c8664e5cd81b00f1695ff1f0f35f85c20b6958
june lee <melodysdreamj@gmail.com>
2024-03-30 14:38:39 +0900

    `

--- bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
+++ bin/commander/build_view/function/remove_action_or_event_import_on_view/function.dart
@@ -1,18 +1,22 @@
 import 'dart:io';
 
-List<String> removeActionOrEventImportOnView(String fileContents) {
+Future<void> removeActionOrEventImportOnView(String path) async {
   // 파일에서 각 줄을 읽어들입니다.
-  List<String> lines = fileContents.split('\n');
+  List<String> lines = await File(path).readAsLines();
 
   // 'action/' 또는 'event/'로 시작하는 import와 export 구문을 제외하고 필터링합니다.
-  List<String> filteredLines = lines.where((line) =>
-  !(line.startsWith("import 'action/") ||
-      line.startsWith("import 'event/") ||
-      line.startsWith("export 'action/") ||
-      line.startsWith("export 'event/"))
-  ).toList();
+  List<String> filteredLines = lines
+      .where((line) => !(line.startsWith("import 'action/") ||
+          line.startsWith("import 'event/") ||
+          line.startsWith("export 'action/") ||
+          line.startsWith("export 'event/")))
+      .toList();
 
-  print('filteredLines: $filteredLines');
+  // 필터링된 내용으로 파일을 다시 씁니다.
+  await File(path).writeAsString(filteredLines.join('\n'));
+}
 
-  return filteredLines;
+void main() {
+  // 함수 사용 예시
+  removeActionOrEventImportOnView('path/to/your/dart/file.dart');
 }
