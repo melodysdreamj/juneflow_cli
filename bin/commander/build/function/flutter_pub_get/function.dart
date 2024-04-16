d4a4575a19e775677088a3e0309d4c62447354d9
june lee <melodysdreamj@gmail.com>
2024-04-16 17:05:18 +0900

    Update function.dart

--- bin/commander/build/function/flutter_pub_get/function.dart
+++ bin/commander/build/function/flutter_pub_get/function.dart
@@ -3,9 +3,10 @@ import 'dart:io';
 // flutter pub get을 실행하는 함수
 Future<void> runFlutterPubGet() async {
   var currentDirectory = Directory.current.path;
-  String flutterCommand = 'flutter.bat';  // Windows에서는 'flutter.bat' 사용
+  String flutterCommand = 'flutter';  // 기본 커맨드 설정
 
   if (Platform.isWindows) {
+    print("Windows 환경에서 실행 중");
     var whereResult = await Process.run('where', ['flutter.bat']);
     if (whereResult.exitCode == 0) {
       flutterCommand = (whereResult.stdout as String).split('\n').first.trim();
@@ -13,6 +14,8 @@ Future<void> runFlutterPubGet() async {
       print('Flutter not found in your path.');
       return;
     }
+  } else {
+    print("비Windows 환경에서 실행 중: ${Platform.operatingSystem}");
   }
 
   var result = await Process.run(flutterCommand, ['pub', 'get'], workingDirectory: currentDirectory);
