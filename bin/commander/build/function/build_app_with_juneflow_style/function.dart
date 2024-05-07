44de539513d2345a71097e9aed7aaa1b4b67d325
june lee <melodysdreamj@gmail.com>
2024-05-07 13:30:17 +0900

    `

--- bin/commander/build/function/build_app_with_juneflow_style/function.dart
+++ bin/commander/build/function/build_app_with_juneflow_style/function.dart
@@ -4,6 +4,7 @@ import '../build_app.run_app/function.dart';
 import '../ready_function.after_material_app/function.dart';
 import '../ready_function.before_material_app/function.dart';
 import '../ready_function.before_run_app/function.dart';
+import '../ready_listener/function.dart';
 
 Future<void> buildAppWithJuneFlowStyle() async {
   await findFunctionsAndGenerateFileMaterialApp();
@@ -13,4 +14,6 @@ Future<void> buildAppWithJuneFlowStyle() async {
   await findFunctionsAndGenerateFileAfterMaterialApp();
   await findFunctionsAndGenerateFileBeforeMaterialApp();
   await findFunctionsAndGenerateFileBeforeRunApp();
+
+  await findFunctionsAndGenerateFileReadyListener();
 }
